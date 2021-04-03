bbb06b208cd71aa2dc0e4fa3216f76535d9a233b
Reza Liaghat <Excadurus@gmail.com>
2021-04-03 15:58:13 +0430

    adding state machine

---
+++ Assets/Scripts/Cursor/CursorState.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public abstract class CursorState
+{
+    private Cursor cursor;
+    private CursorStateMachine csm;
+
+    public abstract void Start();
+
+    public abstract void Update();
+
+    public abstract void FixedUpdate();
+
+    public abstract void Exit();
+}
+
---
+++ Assets/Scripts/Cursor/CursorStateMachine.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class CursorStateMachine
+{
+    private Cursor cursor;
+    private CursorState PlayerActionState { get; set;}
+    private CursorState AIActionState { get; set; }
+
+    private CursorState currentState;
+
+    public CursorStateMachine(Cursor cursor)
+    {
+        this.cursor = cursor;
+        //TODO: Create other States
+    }
+    public void Initialize()
+    {
+        currentState = PlayerActionState;
+        PlayerActionState.Start();
+    }
+
+    public void changeState(CursorState newState)
+    {
+        currentState.Exit();
+        currentState = AIActionState;
+        AIActionState.Start();
+    }
+
+}
